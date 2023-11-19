7f1a1f1cf3c5255e5fa9ad9cdfa33633a44c5458
Faisalbazz <internets420@gmail.com>
2023-11-19 15:50:03 +0700

    commit pertama

---
+++ src/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const UserController = require('../controller/users.js')
+
+const router = express.Router();
+
+// CREATE - POST
+router.get('/', UserController.getAllUsers);
+
+// READ - GET
+router.post('/', UserController.createNewUser);
+
+// UPDATE - PETCH
+router.patch('/:idUser', UserController.updateUser);
+
+// DELETE - DELETE
+router.delete('/:idUser', UserController.deleteUser);
+
+module.exports = router;
