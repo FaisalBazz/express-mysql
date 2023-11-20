0eddb39e46067b9013a2c055f62da7273d3f0dc7
MFaisalBasri <faisal.basri.fbs@gmail.com>
2023-11-20 11:23:35 +0700

    konfigurasi database

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME, // Pilih database yang sesuai
+  host: '34.128.106.232',
+  user: 'root',
+  password: '1111',
+  database: 'express_mysql',
   });
 
   module.exports = dbPool.promise();
